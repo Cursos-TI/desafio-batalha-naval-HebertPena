6b29509e3d7de5a8d794c35418290d09b37bff3d
Hebert Pena <hebertphilipe2@hotmail.com>
2025-02-18 19:07:17 +0000

    AVENTUREIRO CONCLUIDO

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -5,30 +5,27 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+// declaração do tabuleiro 10x10
 int tabuleiro[10][10] = {
     {0,0,0,0,0,0,0,0,0,0},
     {0,3,3,3,0,0,0,0,0,0},
-    {0,0,0,0,3,0,0,0,0,0},
-    {0,0,0,0,3,0,0,0,0,0},
-    {0,0,0,0,3,0,0,0,0,0},
+    {0,0,0,0,0,0,3,0,0,0},
+    {0,0,0,0,0,0,0,3,0,0},
+    {0,0,0,0,0,0,0,0,3,0},
     {0,0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0,0},
+    {0,3,0,0,0,0,0,3,0,0},
+    {0,3,0,0,0,0,3,0,0,0},
+    {0,3,0,0,0,3,0,0,0,0},
     {0,0,0,0,0,0,0,0,0,0}
 };
 
+// Loop aninhado para printar o tabuleiro no terminal com a posiçlão correta dos navios.
 for (int i = 0; i < 10; i++){
     for (int j = 0; j < 10; j++){
         printf("%d", tabuleiro[i][j]);
     }  
     printf("\n");
 }
-
 printf("\n");
 
     return 0;
